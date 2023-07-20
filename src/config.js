258e3c8491900e909d20f24836e2039908c8c8a1
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2023-07-20 22:32:10 +0200

    config file maybe api without slash?

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = (process.env.NODE_ENV === 'production') ? process.env.PORT + '/api' : 'http://localhost:8000/api';
+export const API_URL = (process.env.NODE_ENV === 'production') ? process.env.PORT + 'api' : 'http://localhost:8000/api';
